c7703ed67e421e19016adadc82e37ce4216bc4cc
Jesse <jesse.matherne@esosolutions.com>
2017-05-19 09:07:55 -0500

    Continued work on the left slider
    Signed-off-by: Jesse <jesse.matherne@esosolutions.com>

--- src/app/components/mtgMain/leftSlider/leftSlider.component.ts
+++ src/app/components/mtgMain/leftSlider/leftSlider.component.ts
@@ -8,5 +8,50 @@ import { Component } from '@angular/core';
 
 export class LeftSliderComponent {
 
+	private items: any[] = [
+		{
+			class: 'fa-cloud',
+			name: 'Host Game',
+			func: function () {
+				console.log(this.name);
+			}
+		},
+		{
+			class: 'fa-group',
+			name: 'Join Game',
+			func: function () {
+				console.log(this.name);
+			}
+		},
+		{
+			class: 'fa-book',
+			name: 'Deck Manager',
+			func: function () {
+				console.log(this.name);
+			}
+		},
+		{
+			class: 'fa-line-chart',
+			name: 'Point Manager',
+			func: function () {
+				console.log(this.name);
+			}
+		},
+		{
+			class: 'fa-pencil-square-o',
+			name: 'Storyboard',
+			func: function () {
+				console.log(this.name);
+			}
+		},
+		{
+			class: 'fa-cog',
+			name: 'Settings',
+			func: function () {
+				console.log(this.name);
+			}
+		}
+	];
+
 	constructor () {}
 }
